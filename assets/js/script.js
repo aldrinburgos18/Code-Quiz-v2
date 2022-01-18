74d01fbbcd983e4412293d813a3189b18a9bed6a
aldrinburgos <burgos.aldrin@gmail.com>
2022-01-17 23:00:15 -0600

    successfully loop through questions array and display choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,20 +51,26 @@ const questions = [
   },
 ];
 
+let button, nextButtonEl, quizPageEl, statusEl;
+
 //set variable for timer
-var time = 76;
 var timerEl = document.querySelector("#timer");
-const timer = function () {
-  setInterval(() => {
+
+var time = 76,
+  timer,
+  counter = function () {
     time--;
     timerEl.innerHTML = time + " seconds remaining";
     if (time <= 0 || timer < 1) {
       timerEl.innerHTML = "Time's up!";
-      //show high score
-      //reset timer
     }
-  }, 1000);
-};
+    timer = setTimeout(function () {
+      counter();
+    }, 1000);
+  };
+
+//set variable for score
+var score = time;
 
 var pageContentEl = document.querySelector(".page-content");
 var startPage = document.querySelector("#start-page");
@@ -79,28 +85,42 @@ var setNextQuestion = function () {
   //remove start page
   startPage.classList.add("hide");
 
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  if (questions.length === currentQuestionIndex) {
+    console.log("show high score");
+  } else {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+  }
 };
 
 var showQuestion = function (question) {
+  quizPageEl = document.createElement("section");
+
   var questionEl = document.createElement("div");
   questionEl.setAttribute("id", "question");
   questionEl.innerText = question.question;
-  quizPage.appendChild(questionEl);
+  quizPageEl.appendChild(questionEl);
 
   var answersEl = document.createElement("div");
   answersEl.setAttribute("id", "answer-buttons");
   answersEl.classList.add("btn-grid");
 
-  var nextButtonEl = document.createElement("button");
+  nextButtonEl = document.createElement("button");
   nextButtonEl.setAttribute("id", "next-btn");
-  nextButtonEl.classList.add("next-btn");
+  nextButtonEl.classList.add("next-btn", "hide");
   nextButtonEl.innerText = "Next";
+
+  nextButtonEl.addEventListener("click", () => {
+    currentQuestionIndex++;
+    quizPage.removeChild(quizPageEl);
+    counter();
+    setNextQuestion();
+  });
 
   question.answers.forEach((answer) => {
     //create button for each choices
-    const button = document.createElement("button");
+    button = document.createElement("button");
     button.innerText = answer.option;
+    button.setAttribute("id", "choices");
     button.classList.add("btn");
     //check if answer is correct
     if (answer.correct) {
@@ -108,17 +128,45 @@ var showQuestion = function (question) {
     }
     button.addEventListener("click", selectAnswer);
     answersEl.appendChild(button);
-    quizPage.appendChild(answersEl);
-    quizPage.appendChild(nextButtonEl);
+    quizPageEl.appendChild(answersEl);
+    quizPageEl.appendChild(nextButtonEl);
+    quizPage.appendChild(quizPageEl);
   });
 };
 
-var selectAnswer = function () {};
+var selectAnswer = function (e) {
+  var selectedButton = e.target;
+  var correct = selectedButton.dataset.correct;
+  var buttons = document.getElementsByClassName("btn");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+  clearTimeout(timer);
+
+  statusEl = document.createElement("div");
+  statusEl.classList.add("indicator");
+  if (!correct) {
+    time -= 10;
+    score = time;
+    selectedButton.classList.add("wrong");
+    nextButtonEl.classList.remove("hide");
+    statusEl.innerText = "Incorrect!";
+    quizPageEl.appendChild(statusEl);
+    console.log(time);
+  } else {
+    console.log("correct");
+    nextButtonEl.classList.remove("hide");
+    selectedButton.classList.add("correct");
+    statusEl.innerText = "Correct!";
+    quizPageEl.appendChild(statusEl);
+  }
+};
 
 var startQuiz = function () {
   //start countdown
-  timer();
+  counter();
 
+  //suffle questions
   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
 
